1d9dadb3c8e1f6960e7d6141c2ad93020a1539a1
Hannah Shine <hshine@visitationacademy.org>
2018-06-20 21:16:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ const newID = 1000000000 + oldID;
 const ageIsValid = Number.isInteger(currentAge);
 const randomNumber=Math.random()*(20) + 0;
 const randomInteger=Math.floor(randomNumber);
+constant randomUserID= Math.floor(randomNumber)+1000000000;
