ae05be1440ea24f326cf8ca95d4dde41a1934cea
Hannah Shine <hshine@visitationacademy.org>
2018-06-20 21:04:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ You should NOT try to redefine any of the above variables.
 // Write your code below this line!
 const newID = 1000000000 + oldID;
 const ageIsValid = '{currentAge}.isinteger';
+
+if (isRaining === true) {
+   console.log("It's raining.") //result if the condition is true
+} else if (isRaining === false) {
+   console.log("It's not raining.") //result if the condition is false
+} else {
+   console.log("Invalid input.")
